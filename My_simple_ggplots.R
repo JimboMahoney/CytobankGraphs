3ab847005a746ffb814f8119a978f4bd4bcc32fc
James <JimboMahoney@users.noreply.github.com>
2019-05-29 15:18:54 +0100

    Improvements
    Changed marker names from those imported by the read.FCS function to those pulled from parameters "desc" function
    
    Improved layout / comments.

--- My_simple_ggplots.R
+++ My_simple_ggplots.R
@@ -1,17 +1,14 @@
+# Data Import
+# Specify the data directory here:
+setwd("C:/Users/3054270/Documents/Machines/Helios/Ricardo")
 
 library(flowCore)
 
-# import the data 
-# this read.FCS() function imports the flow data
-setwd("C:/Users/3054270/Documents/Machines/Helios/Cassie")
-raw_fcs<-read.FCS("OP01_ILC01_190516_cat.fcs", alter.names = TRUE)
-
-# Read in panel info from Excel file
-#library(readxl)
-#setwd("C:/Users/3054270/Documents/Tech Documents/Fluidigm Helios CyTOF/Sample Prep")
-#markers <- read_excel("mouse_ab_cytof.xlsx")
+# this read.FCS() function imports the flow data:
+raw_fcs<-read.FCS("Lung_HH_01.fcs", alter.names = TRUE)
 
 
+# Preparation work for arcsinh transform
 # Create list of parameters
 columns<-colnames(raw_fcs)
 # Remove "Time" column to avoid it being transformed
@@ -20,25 +17,34 @@ columns<-setdiff(columns,"Time")
 columns<-setdiff(columns,"Event_length")
 
 
+# Read data into a data frame
+FCSDATA <- as.data.frame(exprs(raw_fcs))
 
+############ Optional Data Transform section
+
+#Remove comments from code lines to transform using asinh
 ## Automatically estimate the logicle transformation based on the data
 #lgcl <- estimateLogicle(raw_fcs, channels = c(columns))
 ## transform  parameters using the estimated logicle transformation
 #raw_fcs_trans <- transform(raw_fcs, lgcl)
 # Load into data frame
 #FCSDATA <- as.data.frame(exprs(raw_fcs_trans))
-# OR
-# Don't transform - just load into data frame
-FCSDATA <- as.data.frame(exprs(raw_fcs))
 
-#Remove unnecessary parameter text
-names(FCSDATA)[-1] <- sub("Di", "", names(FCSDATA)[-1])
+########### End of optional Data Transform section
 
+
+
+#Remove unnecessary parameter text - superseded by next function
+#names(FCSDATA)[-1] <- sub("Di", "", names(FCSDATA)[-1])
+# Create list of descriptions 
+params<-parameters(raw_fcs)[["desc"]]
+# Replace parameters with descriptions, keeping things like Time, Event Length unchanged
+colnames(FCSDATA)[!is.na(params)] <- na.omit(params)
 
 #Subsample using random 10% of original rows
 #FCSDATA <- FCSDATA[sample(nrow(FCSDATA),nrow(FCSDATA)/10),]
 #OR
-#Subsample using random x original rows
+#Subsample using a number of random rows, where the number is defined by numrows
 numrows <- 1000
 FCSDATA <- FCSDATA[sample(nrow(FCSDATA),numrows),]
 
@@ -47,11 +53,13 @@ FCSDATA <- FCSDATA[sample(nrow(FCSDATA),numrows),]
 library(reshape2)
 fcsmelted <- melt(FCSDATA, id.var="Time", value.name = "intensity", variable.name="parameter")
 
+
+
 #use ggplot2 to draw dot plot
 library(ggplot2)
 
 
-# Create colour scale
+# Create colour scale for plot
 # The extra "black" is needed to make the colour scale appear decent - otherwise it doesn't show
 colfunc <- colorRampPalette(c("black", "black","black", "black", "black", "black", "black", "black",
                               "purple4", "purple4", 
@@ -63,9 +71,10 @@ div = (60*1000)
 
 # For rounding time to a nice number on the x axis graphs
 maxtime<-round(max(fcsmelted$Time)/div)
+max(fcsmelted$Time)
 
 
-
+## Plot x as Time and Y as intensity
 ggplot(fcsmelted, aes(x=Time/div, y=intensity)) +
   # Only label 0 and max on X Axis
   scale_x_continuous(breaks=seq(0,maxtime,maxtime)) +
@@ -76,7 +85,7 @@ ggplot(fcsmelted, aes(x=Time/div, y=intensity)) +
   stat_density2d(geom="raster", aes(fill=..ndensity.., alpha = ..ndensity..), contour = FALSE) +
   # Produces a colour scale based on the colours in the colfunc list
   scale_fill_gradientn(colours=colfunc(128)) + 
-  # Contour lines
+  # Contour lines if desired
   # geom_density2d(colour="black", bins=3)  +  
   # Repeat for all parameters and allow each to be on their own Y axis scale
   # Note that free scales increases the processing time substantially
@@ -85,10 +94,11 @@ ggplot(fcsmelted, aes(x=Time/div, y=intensity)) +
   ylim(0,NA) +
   # And scale y to log, displaying numbers rather than notation
   scale_y_log10(labels=scales::comma) + 
-  # Hide Y axis values
+  # Hide Y axis values if desired
   #theme(axis.text.y = element_blank(), axis.ticks = element_blank()) +
   # Hide legend
   theme(legend.position = "none") +
   # Hide Y axis label
   ylab(NULL) +
+  # Change X axis label
   xlab("Time (min)")
